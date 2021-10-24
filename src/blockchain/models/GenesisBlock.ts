b037777f62aa537fb2f696ee4a515936a9703f55
Patryk Sadok <patryk.sadok@gmail.com>
2021-10-24 16:15:06 +0200

    feat: genesis block

--- src/blockchain/models/GenesisBlock.ts
+++ src/blockchain/models/GenesisBlock.ts
@@ -1,7 +1,7 @@
 import { Block } from "./Block";
 import { GENESIS_DATA } from "../../config/constants";
 
-export class GenesisBlock {
+export class GenesisBlock extends Block {
   static create(): Block {
     return new Block({
       lastHash: GENESIS_DATA.lastHash,
