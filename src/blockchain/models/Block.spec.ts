a7da8fe195ee6d6eed607cdb196140ebeed41459
Patryk Sadok <patryk.sadok@gmail.com>
2021-10-24 16:27:09 +0200

    TDD: block.mineblock tests

--- src/blockchain/models/Block.spec.ts
+++ src/blockchain/models/Block.spec.ts
@@ -1,4 +1,5 @@
 import { Block } from "./Block";
+import { GenesisBlock } from "./GenesisBlock";
 
 describe("Block model tests", () => {
   describe("constructor", () => {
@@ -13,6 +14,28 @@ describe("Block model tests", () => {
       expect(block.hash).toEqual("451");
       expect(block.data).toEqual({ exampleKey: "exampleValue" });
       expect(block.timestamp).not.toBeNull();
+    });
+  });
+
+  describe("mineBlock", () => {
+    const lastBlock = GenesisBlock.create();
+
+    const data = "mined data";
+    const mineBlock = Block.mineBlock({ lastBlock, data });
+
+    it("creates new Block", () => {
+      expect(mineBlock instanceof Block).toEqual(true);
+    });
+    it("holds reference to the previous block", () => {
+      expect(mineBlock.lastHash).toEqual(lastBlock.hash);
+    });
+    it("sets the data correctly", () => {
+      expect(mineBlock.data).toEqual(data);
+    });
+    it("sets a timestamp", () => {
+      expect(mineBlock.timestamp instanceof Date);
+      expect(mineBlock.timestamp).not.toBeNull();
+      expect(mineBlock.timestamp).not.toBeUndefined();
     });
   });
 });
